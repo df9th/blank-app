df6da7f909af32a5aa122dca05b677f8d6c77743
Paulo Sérgio Santos Morais <153513318+df9th@users.noreply.github.com>
2025-11-21 00:07:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -162,10 +162,10 @@ if file is not None:
 
 
         st.subheader("Boxplot dos Pesos")
-            box = px.box(
-                y=valores,
-                labels={"y": "Peso (g)"},
-                title="Boxplot dos Pesos",
+        box = px.box(
+            y=valores,
+            labels={"y": "Peso (g)"},
+            title="Boxplot dos Pesos",
             )
         st.plotly_chart(box, use_container_width=True)
 
